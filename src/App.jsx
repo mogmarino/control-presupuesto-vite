7f54840a4c5fad63330c4b721f25b1817c046edc
mogmarino <mogmarino@hotmail.com>
2022-04-02 19:30:27 -0300

    comenzando

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Control de gastos</h1>
+    </div>
+  );
+}
+
+export default App;

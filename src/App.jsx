249c30b5c1b74b864a09895ffa11f1f373842c42
mogmarino <mogmarino@hotmail.com>
2022-04-02 21:50:29 -0300

    progreso del sabado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import { useState } from "react";
+import Header from "./components/Header";
+import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [presupuesto, setPresupuesto] = useState(0);
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
 
   return (
     <div className="App">
-      <h1>Control de gastos</h1>
+      <Header
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+        isValidPresupuesto={isValidPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}
+      />
+
+      {isValidPresupuesto && (
+        <div className="nuevo-gasto">
+          <img src={IconoNuevoGasto} alt="icono nuevo gasto" />
+        </div>
+      )}
     </div>
   );
 }
